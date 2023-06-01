1b0d96d393938e1db64c8bf5a58c5a6bcdbe10d1
Hieu <dev.ngtronghieu@gmail.com>
2023-06-01 23:24:00 +0700

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use magic_crypt::{new_magic_crypt, MagicCryptTrait};
+use sha256::digest;
+
+fn encrypt(seedphrase: &str, password: &str) -> String {
+    let key = digest(password);
+    let mc = new_magic_crypt!(key, 256);
+    let encrypted_str = mc.encrypt_str_to_base64(seedphrase);
+    encrypted_str
+}
+
+fn decrypt(encrypted_seedphrase: &str, password: &str) -> String {
+    let key = digest(password);
+    let mc = new_magic_crypt!(key, 256);
+    let decrypted_str = mc.decrypt_base64_to_string(encrypted_seedphrase).unwrap();
+    decrypted_str
+}
+
 fn main() {
-    println!("Hello, world!");
+    let seedphrase = "Insert your seed phrase here";
+    let password = "Your password here";
+
+    let encrypted_seedphrase = encrypt(seedphrase, password);
+
+    println!("{}", encrypted_seedphrase);
+
+    let decrypted_seedphrase = decrypt(&encrypted_seedphrase, password);
+
+    println!("{}", decrypted_seedphrase);
 }
