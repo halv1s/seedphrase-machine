3a2ee081ab22a702eaf3ee11fc23a905be9d61cc
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-06-10 09:14:34 +0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,13 @@ use sha256::digest;
 fn encrypt(seedphrase: &str, password: &str) -> String {
     let key = digest(password);
     let mc = new_magic_crypt!(key, 256);
-    let encrypted_str = mc.encrypt_str_to_base64(seedphrase);
-    encrypted_str
+    mc.encrypt_str_to_base64(seedphrase)
 }
 
 fn decrypt(encrypted_seedphrase: &str, password: &str) -> String {
     let key = digest(password);
     let mc = new_magic_crypt!(key, 256);
-    let decrypted_str = mc.decrypt_base64_to_string(encrypted_seedphrase).unwrap();
-    decrypted_str
+    mc.decrypt_base64_to_string(encrypted_seedphrase).unwrap()
 }
 
 fn main() {
